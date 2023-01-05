fd52c67eaf4bc20eae67d7114a01fafda9aa6144
iconicspidey <64977181+iconicspidey@users.noreply.github.com>
2023-01-05 11:46:39 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// AOS.init({
+//      duration: 800,
+//     easing: 'ease-in-quad',
+//     delay: 100,
+// });
+const myModal = document.querySelector(".mymodal");
+
+window.addEventListener("onload", () => {
+  myModal.style.display = "none";
+});
